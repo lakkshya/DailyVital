bf0e4d66dfae1bf87a7facb416f8c8459c4d0f9a
Lakshya Rana <work.lakshyarana@gmail.com>
2024-09-15 14:23:19 +0530

    ui done

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:daily_vital/models/appuser.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
@@ -11,9 +12,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String userName = '';
   String dayTime = 'Day';
+  String todayDate = '';
 
   Future<void> fetchUserName(String uid) async {
     try {
@@ -31,36 +32,280 @@ class _HomeState extends State<Home> {
 
   void setDayTimeGreeting() {
     final hour = DateTime.now().hour;
-    if(hour < 12) {
+    if (hour < 12) {
       dayTime = 'morning';
-    }
-    else if(hour < 17) {
+    } else if (hour < 17) {
       dayTime = 'afternoon';
-    }
-    else {
+    } else {
       dayTime = 'evening';
     }
+  }
+
+  void setTodayDate() {
+    final now = DateTime.now();
+    final formatter = DateFormat('EEEE, MMMM d');
+    setState(() {
+      todayDate = formatter.format(now);
+    });
   }
 
   @override
   void initState() {
     super.initState();
     setDayTimeGreeting();
+    setTodayDate();
     final user = Provider.of<AppUser>(context, listen: false);
     fetchUserName(user.uid);
-    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: const EdgeInsets.fromLTRB(30.0, 70.0, 30.0, 0.0),
+      padding: const EdgeInsets.fromLTRB(30.0, 60.0, 30.0, 0.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text(
+            todayDate,
+            style: const TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
           Text(
             'Good $dayTime, $userName',
             style: const TextStyle(
               fontSize: 24.0,
             ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(60.0, 50.0, 60.0, 50.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: const AssetImage('assets/intro.png'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.blue.withOpacity(0.3),
+                  BlendMode.srcOver,
+                ),
+              ),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Scan your prescription to set reminders for your medications. This ensures you never miss a dose.',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'My schedule',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120.0,
+                      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(children: [
+                        const Text(
+                          '11:00',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          '2 pills',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Icon(
+                            Icons.done_rounded,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ]),
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Container(
+                      width: 120.0,
+                      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(children: [
+                        const Text(
+                          '11:00',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          '2 pills',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Icon(
+                            Icons.done_rounded,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ]),
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Container(
+                      width: 120.0,
+                      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(children: [
+                        const Text(
+                          '11:00',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          '2 pills',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Icon(
+                            Icons.done_rounded,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ]),
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Container(
+                      width: 120.0,
+                      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(children: [
+                        const Text(
+                          '11:00',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          '2 pills',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Icon(
+                            Icons.done_rounded,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('See full'),
+                ),
+              ),
+            ],
           ),
         ],
       ),
--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -11,18 +11,57 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   final AuthService _auth = AuthService();
 
+  final String name = "John Doe";
+  final String age = "30";
+  final String contactNumber = "+1234567890";
+  final String gender = "Male";
+  final String avatarUrl = 'assets/man.png';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(30.0, 150.0, 30.0, 0.0),
-      child: Center(
-        child: ElevatedButton.icon(
-          onPressed: () async {
-            await _auth.signOut();
-          },
-          label: const Text('logout'),
-          icon: const Icon(Icons.logout),
-        ),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.fromLTRB(30.0, 60.0, 30.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CircleAvatar(
+            radius: 60.0,
+            backgroundImage: AssetImage(avatarUrl),
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            name,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          Text(
+            'Age: $age',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          Text(
+            'Contact: $contactNumber',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          Text(
+            'Gender: $gender',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          const SizedBox(height: 30.0),
+          ElevatedButton.icon(
+            onPressed: () async {
+              await _auth.signOut();
+            },
+            label: const Text('logout'),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
     );
   }
